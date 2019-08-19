d32356f537a555389591cf111858d069819f1737
Danilo Imparato <imparatodanilo@gmail.com>
2019-08-19 04:45:56 -0300

    settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="settings flex flex-col h-full p-4 text-left">
+    
+        <label class="block text-gray-500 text-md font-bold mb-2 uppercase">Avaliação</label>
+    <div class="bg-white shadow rounded p-6 mb-4 text-left">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username</label>
+        <div class="inline-block relative w-64">
+  <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+    <option>Really long option that will likely overlap the chevron</option>
+    <option>Option 2</option>
+    <option>Option 3</option>
+  </select>
+  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+  </div>
+</div>
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+        <input
+          class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          placeholder="******************"
+        />
+        <p class="text-red-500 text-xs italic">Please choose a password.</p>
+      </div>
+      <div class="flex items-center justify-between">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+        >Sign In</button>
+        <a
+          class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          href="#"
+        >Forgot Password?</a>
+      </div>
+    </div>
   </div>
 </template>
